7e13438faa043dc05697c16644b665a48e727157
cccccc1 <cccccc1@5582e8ba-6f53-4c69-84e8-958b9bb8e6d1>
2013-01-21 03:47:28 +0000

    Updated Javadoc.

--- APSimSDK/src/com/prodp/apsim/plugins/FunctionHelper.java
+++ APSimSDK/src/com/prodp/apsim/plugins/FunctionHelper.java
@@ -1,5 +1,17 @@
 package com.prodp.apsim.plugins;
 
+/**
+ * 
+ * This function is the "lifeline" to the APSim project.
+ * 
+ * In the SDK version, this class does not contain any real functions, more like
+ * a shell.
+ * 
+ * @version 0.0
+ * @author Jonathan
+ * 
+ */
+
 public class FunctionHelper {
 
 }
